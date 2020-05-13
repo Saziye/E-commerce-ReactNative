43db63bdfaacd480b81175b528dd886bcd767918
Saz <saziye@gmail.com>
2020-05-13 17:53:50 +0300

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {ActivityIndicator} from 'react-native';
+import { UtilityThemeProvider, Box} from 'react-native-design-utility';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import Navigation from './src/screens'
+
+import {images} from './src/constants/images';
+import {cacheImages} from './src/utils/cacheImages';
+
+export default class App extends React.Component {
+  state = {
+    isReady: false,
+  };
+
+  componentDidMount() {
+    this.cacheAssets();
+  }
+
+  cacheAssets = async () => {
+    const imageAssets = cacheImages(Object.values(images));
+
+    await Promise.all([...imageAssets]);
+
+    this.setState({isReady:true});
+  };
+
+  render() {
+    if(!this.state.isReady) {
+      return (
+        <Box f={1} center bg="white">
+          <ActivityIndicator size="large"/>
+        </Box>
+      )
+    }
+    return (
+      <UtilityThemeProvider>
+        <Navigation />
+      </UtilityThemeProvider>
+    );
+  }
+  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
